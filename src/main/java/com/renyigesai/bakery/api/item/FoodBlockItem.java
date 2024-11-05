d033a91e18367b9511ec14b72403fd8fed42f512
Renyigesai <2907654918@qq.com>
2024-11-05 14:46:34 +0800

    添加提示信息2024/11/4

--- src/main/java/com/renyigesai/bakery/api/item/FoodBlockItem.java
+++ src/main/java/com/renyigesai/bakery/api/item/FoodBlockItem.java
@@ -1,22 +1,46 @@
 package com.renyigesai.bakery.api.item;
 
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
 import com.renyigesai.bakery.api.block.PileBlock;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffectUtil;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemNameBlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class FoodBlockItem extends ItemNameBlockItem {
+
+    private static final Component NO_EFFECT = Component.translatable("effect.none").withStyle(ChatFormatting.GRAY);
+
     public FoodBlockItem(Block block, Properties pProperties) {
         super(block, pProperties);
     }
@@ -63,5 +87,37 @@ public class FoodBlockItem extends ItemNameBlockItem {
         }
         return InteractionResult.SUCCESS;
 
+    }
+
+    private static void listPotionEffects(ItemStack pStack, Consumer<MobEffectInstance> pOutput) {
+        CompoundTag compoundtag = pStack.getTag();
+        if (compoundtag != null && compoundtag.contains("Effects", 9)) {
+            ListTag listtag = compoundtag.getList("Effects", 10);
+
+            for(int i = 0; i < listtag.size(); ++i) {
+                CompoundTag compoundtag1 = listtag.getCompound(i);
+                int j;
+                if (compoundtag1.contains("EffectDuration", 99)) {
+                    j = compoundtag1.getInt("EffectDuration");
+                } else {
+                    j = 160;
+                }
+
+                MobEffect mobeffect = MobEffect.byId(compoundtag1.getInt("EffectId"));
+                mobeffect = net.minecraftforge.common.ForgeHooks.loadMobEffect(compoundtag1, "forge:effect_id", mobeffect);
+                if (mobeffect != null) {
+                    pOutput.accept(new MobEffectInstance(mobeffect, j));
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel,List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+            List<MobEffectInstance> list = new ArrayList<>();
+            listPotionEffects(pStack, list::add);
+            PotionUtils.addPotionTooltip(list, pTooltipComponents, 1.0F);
     }
 }
