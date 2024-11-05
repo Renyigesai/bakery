4b7181dd170c815306e5e5ec56f5276a8381cab8
Renyigesai <2907654918@qq.com>
2024-11-05 14:47:10 +0800

    调整饱食度，添加药水效果2024/11/4

--- src/main/java/com/renyigesai/bakery/init/BakeryFoodProperties.java
+++ src/main/java/com/renyigesai/bakery/init/BakeryFoodProperties.java
@@ -1,5 +1,8 @@
 package com.renyigesai.bakery.init;
 
+import com.mojang.blaze3d.shaders.Effect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class BakeryFoodProperties {
@@ -14,14 +17,26 @@ public class BakeryFoodProperties {
     public static final FoodProperties SLICED_TOAST;
 
     static {
-        BAGEL = new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build();
+        BAGEL = new FoodProperties.Builder().nutrition(4).saturationMod(1.25f).build();
+
         BAGUETTE = new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build();
-        CINNAMON_ROLL = new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build();
+
+        CINNAMON_ROLL = new FoodProperties.Builder().nutrition(3).saturationMod(1.3f)
+                .effect(new MobEffectInstance(MobEffects.REGENERATION, 200, 0), 1.0F).build();
+
         COUNTRY_BREAD = new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build();
-        CROISSANT = new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build();
-        PINEAPPLE_BUN = new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build();
-        ROUND_BREAD = new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build();
-        SALT_CROISSANT = new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build();
+
+        CROISSANT = new FoodProperties.Builder().nutrition(3).saturationMod(3.0f).build();
+
+        PINEAPPLE_BUN = new FoodProperties.Builder().nutrition(3).saturationMod(1.3f)
+                .effect(new MobEffectInstance(MobEffects.REGENERATION, 200, 0), 1.0F).build();
+
+        ROUND_BREAD = new FoodProperties.Builder().nutrition(3).saturationMod(0.6f).build();
+
+        SALT_CROISSANT = new FoodProperties.Builder().nutrition(3).saturationMod(2.0f)
+                .effect(new MobEffectInstance(MobEffects.REGENERATION, 200, 0), 1.0F)
+                .effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 600, 0), 1.0F).build();
+
         SLICED_TOAST = new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build();
     }
 }
