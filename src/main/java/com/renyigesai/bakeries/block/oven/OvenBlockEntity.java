0d28a7807afe101f8a8ce4a8017a1c9ae405fafa
1-weibai-1 <1916423401@qq.com>
2025-01-17 13:04:22 +0800

    声音ok就是要靠进点。 2025.01.17

--- src/main/java/com/renyigesai/bakeries/block/oven/OvenBlockEntity.java
+++ src/main/java/com/renyigesai/bakeries/block/oven/OvenBlockEntity.java
@@ -1,7 +1,6 @@
 package com.renyigesai.bakeries.block.oven;
 
 import com.renyigesai.bakeries.init.BakeriesBlocks;
-import com.renyigesai.bakeries.init.BakeriesSounds;
 import com.renyigesai.bakeries.inventory.oven.OvenMenu;
 import com.renyigesai.bakeries.recipe.oven.OvenRecipe;
 import io.netty.buffer.Unpooled;
@@ -22,12 +21,8 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.ChestBlockEntity;
-import net.minecraft.world.level.block.entity.ContainerOpenersCounter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
@@ -36,7 +31,6 @@ import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.openjdk.nashorn.internal.objects.annotations.Getter;
 
 import java.util.Optional;
 
