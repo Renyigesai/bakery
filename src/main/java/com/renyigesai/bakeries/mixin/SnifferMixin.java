1a59a89277e77348569ee5929c244628e735ab10
renyigesai <2907654918@qq.com>
2026-05-03 11:22:07 +0800

    修复嗅探兽挖掘错误2026/05/03

--- src/main/java/com/renyigesai/bakeries/mixin/SnifferMixin.java
+++ src/main/java/com/renyigesai/bakeries/mixin/SnifferMixin.java
@@ -25,7 +25,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Sniffer.class)
 public abstract class SnifferMixin extends Animal {
 
-    protected abstract BlockPos getHeadBlock();
+    @Shadow protected abstract BlockPos getHeadBlock();
 
     protected SnifferMixin(EntityType<? extends Animal> p_27557_, Level p_27558_) {
         super(p_27557_, p_27558_);
