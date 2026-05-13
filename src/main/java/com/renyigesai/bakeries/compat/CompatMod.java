78debda14c7143cd2731683af4e7743f268f56af
gengyoubo <gengyoubo@gmail.com>
2026-05-14 00:40:33 +0900

    更新物品

--- src/main/java/com/renyigesai/bakeries/compat/CompatMod.java
+++
@@ -1,9 +0,0 @@
-package com.renyigesai.bakeries.compat;
-
-import net.minecraftforge.fml.ModList;
-
-public record CompatMod() {
-    /*记录联动模组的ModId*/
-    public static final boolean FARMER_S_DELIGHT = ModList.get().isLoaded("farmersdelight");//农夫乐事
-    public static final boolean KALEIDOSCOPE_COOKERY = ModList.get().isLoaded("kaleidoscope_cookery");//森罗物语：厨房
-}
